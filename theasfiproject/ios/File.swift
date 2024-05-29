907a15e5d38cc968aa3714efc21e6f9b0eb3c3ea
Benson Michael Owen <lordkz19@gmail.com>
2024-05-29 23:54:26 +0100

    New Look ad Functions for ASFI scholar

--- theasfiproject/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  theasfiproject
-//
-
-import Foundation
--- theasfiproject/src/subComponents/SidePanelEnum.tsx
+++
@@ -1,9 +0,0 @@
-
-export enum SidePanelType {
-  None,
-  Participants,
-  Chat,
-  Settings,
-  PosterDecks,
-  Polls
-}
